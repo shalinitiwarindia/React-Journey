ad46d40cb056f4404e3dcbe98fc525ec75c4be00
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-25 15:08:50 +0530

    Update index.js

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,11 @@
 import {sum,mult} from "./calc";
-
+import "./index.css";
 
 console.log(sum(6,5));
 console.log(mult(4,5));
+
+const root = document.getElementById("root");
+const h1 = document.createElement("h1");
+h1.innerText = "Hello World";
+h1.classList.add("redtext");
+root.appendChild(h1);
